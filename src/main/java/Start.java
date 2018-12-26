ac4b74d539ec548c6d49ca85aad22f97751f2ebd
ILoskutnikov <i.a.loskutnikov@gmail.com>
2018-12-26 20:18:22 +0300

    [TEST3]

--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -2,5 +2,6 @@ public class Start {
     public static void main(String[] args) {
         System.out.println("Start");
         System.out.println("TEST");
+        System.out.println("TEST2");
     }
 }
